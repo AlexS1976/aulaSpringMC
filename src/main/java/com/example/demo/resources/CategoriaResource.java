e0a8b1186c06362e543e2dec00ef9957fe6e0be1
Alex Silva <damasioesilva@gmail.com>
2021-07-27 01:31:32 -0300

    end points testados

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -3,7 +3,9 @@ package com.example.demo.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,11 @@ import com.example.demo.domain.services.CategoriaService;
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
+	@Autowired
 	private CategoriaService service;
-		@RequestMapping(value="/(id)",method = RequestMethod.GET)
-		public ResponseEntity<?> find(Integer id) {
+	
+		@RequestMapping(value="/{id}",method = RequestMethod.GET)
+		public ResponseEntity<?> find(@PathVariable Integer id) {
 			
 			Categoria obj = service.buscar(id);
 			return ResponseEntity.ok().body(obj);
